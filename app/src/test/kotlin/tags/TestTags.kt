3b9f1d66fa5d3f811439e34432e33e1269dafc38
Anouar Doukkali <anouar.doukkali@gmail.com>
2024-04-19 12:42:56 +0100

    kotest Tags

---
+++ app/src/test/kotlin/tags/TestTags.kt
@@ -0,0 +1,17 @@
+/**
+ * File: TestTags.kt
+ * Author: Anouar Doukkali
+ * Created on: 4/19/2024 4:51 AM
+ * Description:
+ * Since: v0.1.0
+ */
+package tags
+
+import io.kotest.core.Tag
+
+internal object Windows : Tag()
+internal object Linux : Tag()
+internal object Android : Tag()
+internal object IOS : Tag()
+internal object MacOS : Tag()
+internal object UnitTest : Tag()
